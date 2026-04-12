00a03b4f0728a33abd0b08879aa13bb718d77300
Moritz Bärnt <moritzbaernt@outlook.de>
2026-04-12 19:52:11 +0200

    Implement TODO scanner.
    Implement file and Todo classes.
    Detect supportet files based on their extension (for now .py and .tex).
    Parse comment lines for "TODO:" entries and output them with file an line information.

---
+++ what-todo.py
@@ -0,0 +1,121 @@
+import os
+import sys
+
+from dataclasses import dataclass
+
+@dataclass
+class Todo:
+    """
+    Describes a todo.
+    """
+    file: str
+    line: int
+    description: str
+
+    def location(
+        self,
+    ) -> str:
+        """
+        Returns the todos location in the form of [file]@[line].
+
+        :returns: str
+        """
+        return f"{self.file}@{self.line}"
+
+@dataclass
+class File:
+    """
+    Describes a file.
+    """
+    path: str
+    name: str
+    extension: str
+    comment_specifier: str
+
+supported_file_extensions: list[str] = [
+    "tex",
+    "py",
+]
+
+comment_specifiers: dict[str, str] = {
+    "tex": "%",
+    "py": "#",
+}
+
+def get_supported_files(
+    directory: str,
+) -> list[File]:
+    """
+    Get a list with supported files in the directory.
+
+    Checks the file extension of files in the directory.
+    Builds a list with files that have supported extensions.
+
+    :param directory: Path to the directory to look into.
+    :type directory: str
+
+    :returns: list[File]
+    """
+    file_names: list[str] = os.listdir(directory)
+    
+    file_names = [f for f in file_names if f.split(".")[-1] in supported_file_extensions]
+
+    files: list[File] = [File(path=os.path.join(directory, f), name=f, extension=f.split(".")[-1], comment_specifier=comment_specifiers[f.split(".")[-1]]) for f in file_names]
+
+    return files
+
+def get_todos_from_file(
+    file: File,
+) -> list[Todo]:
+    """
+    Get a list with todos in a file.
+
+    Iterates over the files content and checks comments for todos.
+    Builds a list with all todos that are found.
+
+    :param file: File
+    :type file: File
+
+    :returns: list[Todo] 
+    """
+    todos: list[Todo] = []
+
+    line_counter: int = 0
+    with open(file.path, "r") as f:
+        for line in f:
+            line_counter = line_counter + 1
+
+            # We are only looking for todos in comments. -> Ignore line without comments.
+            if not line.startswith(file.comment_specifier):
+                continue
+            
+            # Line has no todo. -> Skip.
+            if not "TODO:" in line:
+                continue
+
+            todos.append(Todo(file=file.name, line=line_counter, description=line))
+
+    return todos
+
+if __name__ == "__main__":
+    directory: str = ""
+    if len(sys.argv) <= 1:
+        directory = os.getcwd()
+    else:
+        directory = sys.argv[1]
+    
+    if not os.path.isdir(directory):
+        raise ValueError(f"Path does not exist: {directory}")
+    
+    files: list[File] = get_supported_files(directory)
+
+    for f in files:
+        todos: list[Todo] = get_todos_from_file(f)
+
+        if not todos:
+            continue
+
+        print(f"TODOs in {f.name}:")
+        
+        for t in todos:
+            print(f"  {t.location()}: {t.description[:-1]}")
